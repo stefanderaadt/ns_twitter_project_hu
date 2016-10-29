86ee0857aadd65403cdd035dded339a6b3c9c773
stefanderaadt <stefanderaadt@gmail.com>
2016-10-29 17:50:00 +0200

    Eerste versie om tweets te posten en ophalen

---
+++ twitter.py
@@ -0,0 +1,43 @@
+import oauth2 as oauth
+import json
+from urllib.parse import quote
+from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET, USER
+
+
+class Twitter:
+
+    def __init__(self):
+
+        consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+        access_token = oauth.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
+
+        self.client = oauth.Client(consumer, access_token)
+
+    def __post(self, url):
+
+        response, data = self.client.request(url, "POST")
+
+    def __get(self, url):
+
+        response, data = self.client.request(url, "GET")
+
+        return data
+
+    def tweet(self, message):
+
+        message = quote(message, safe='')
+        url = "https://api.twitter.com/1.1/statuses/update.json?status="+message
+
+        self.__post(url)
+
+        return
+
+    def feed(self):
+
+        url = "https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=" + USER
+
+        data = self.__get(url)
+
+        tweets = json.loads(data.decode('utf-8'))
+
+        return tweets
