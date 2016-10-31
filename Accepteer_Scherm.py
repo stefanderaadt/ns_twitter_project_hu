124e44c2fcc36e094a6d533b33bf22d33f4244b1
jeffreygek1 <jeffrey.gek@hotmail.com>
2016-10-31 20:45:43 +0100

    Begin van menu toegevoegd en uitlezen CSV mogelijk

--- Accepteer_Scherm.py
+++ Accepteer_Scherm.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import csv
 import twitter
 
+
 class MainMenu(Frame):
     def __init__(self, master):
         super(MainMenu, self).__init__(master)
@@ -15,26 +16,30 @@ class MainMenu(Frame):
 
     def create_GUI(self):
 
-        self.label1 = Label(mainWindow, text=self.MainMessage()[0], width="2000",height = "1", background=self.KleurTweet(), anchor='w')
+        self.label1 = Label(mainWindow, text=self.MainMessage()[0], width="2000", height="1",
+                            background=self.KleurTweet(), anchor='w')
         self.label1.grid(row=0, column=0, sticky=W)
-        self.label2 = Label(mainWindow, text=self.MainMessage()[1], width="2000",height = "1", background=self.KleurTweet(), anchor='w')
+        self.label2 = Label(mainWindow, text=self.MainMessage()[1], width="2000", height="1",
+                            background=self.KleurTweet(), anchor='w')
         self.label2.grid(row=1, column=0, sticky=W)
         for i in range(len(self.TweetOntvangen()[0])):
-            self.button = Button(mainWindow, text=(self.TweetOntvangen()[0][i]+ " ontvangen door, "+ self.TweetOntvangen()[1][i]), command=self.Onpress, width="2000", anchor='w')
+            self.button = Button(mainWindow,
+                                 text=(self.TweetOntvangen()[0][i] + " ontvangen door, " + self.TweetOntvangen()[1][i]),
+                                 command=self.Onpress, width="2000", anchor='w')
 
-            self.button.grid(row=2+i, column=0, sticky=W)
+            self.button.grid(row=2 + i, column=0, sticky=W)
 
     def TweetOntvangen(self):
 
+        with open("data/tweets.csv ", "r") as MyCsvFile:
+            reader = csv.DictReader(MyCsvFile)
+            OntvangenTweets = []
+            GeplaatstDoor = []
+            for row in reader:
+                OntvangenTweets.append(row['tweet'])
+                GeplaatstDoor.append(row["plaatser"])
+        return OntvangenTweets, GeplaatstDoor
 
-            with open("data/tweets.csv ", "r") as MyCsvFile:
-                reader = csv.DictReader(MyCsvFile)
-                OntvangenTweets = []
-                GeplaatstDoor = []
-                for row in reader:
-                    OntvangenTweets.append(row['tweet'])
-                    GeplaatstDoor.append(row["plaatser"])
-            return OntvangenTweets,GeplaatstDoor
     def IsTweetOntvangen(self):
         if len(self.TweetOntvangen()[0]) != 0:
             Tweet = 1
@@ -45,17 +50,16 @@ class MainMenu(Frame):
     def MainMessage(self):
         bericht = ""
         bericht1 = ''
-        if self.IsTweetOntvangen()== 0:
+        if self.IsTweetOntvangen() == 0:
             bericht = 'U heeft geen bericht ontvangen.'
-        if self.IsTweetOntvangen()== 1:
+        if self.IsTweetOntvangen() == 1:
             if len(self.TweetOntvangen()[0]) > 1:
-                bericht = "U heeft "+str(len(self.TweetOntvangen()[0]))+" berichten"
+                bericht = "U heeft " + str(len(self.TweetOntvangen()[0])) + " berichten"
                 bericht1 = "Dit zijn de berichten:"
             else:
                 bericht = "U heeft een bericht."
                 bericht1 = "Dit is uw bericht:"
         return bericht, bericht1
-
 
     def Onpress(self):
         result = messagebox.askquestion("Tweet versturen", "Wilt u deze tweet versturen?", icon="warning")
@@ -64,16 +68,21 @@ class MainMenu(Frame):
         else:
             TweetNaarTwitter = 0
         return TweetNaarTwitter
-
+    def TweetVersturen(self):
+        if self.Onpress() == 1:
+            print("a")
+        else:
+            print("b")
     def FormaatKiezen(self):
         # screen = str(input("screenformaat? Je kunt invullen:\nFullscreen\nFormaat in HxB bijvoorbeeld 1920x1080\n")).lower()
         # if screen == 'fullscreen':
         #     return mainWindow.attributes('-fullscreen', True)
         # else:
-            #return mainWindow.geometry('500x500+1920+200')
-            return mainWindow.geometry('500x500')
+        # return mainWindow.geometry('500x500+1920+200')
+        return mainWindow.geometry('500x500')
+
     def KleurTweet(self):
-        if self.IsTweetOntvangen()== 0:
+        if self.IsTweetOntvangen() == 0:
             global kleur
             kleur = "red"
         else:
@@ -81,11 +90,9 @@ class MainMenu(Frame):
         return kleur
 
 
-
-
-
 mainWindow = Tk()
 mainWindow.title("TweetsCheck MainPanel")
 MainMenu(mainWindow)
-mainWindow.configure(background=kleur)#KleurTweet())
+
+mainWindow.configure(background=kleur)  # KleurTweet())
 mainWindow.mainloop()
