29b72965252ed8dbaf334639a1eb1d937c7d1e42
Stefan <stefanderaadt@gmail.com>
2016-10-31 13:24:22 +0100

    hallo

--- invoerscherm.py
+++ invoerscherm.py
@@ -1,61 +1,49 @@
 from tkinter import *
-import twitter
+from config import CSV_PATH
+import csv
 
 class MainMenu(Frame):
     def __init__(self, master):
         super(MainMenu, self).__init__(master)
 
-        pad = 3
-
-        self._geom='200x200+0+0'
-        master.geometry("{0}x{1}+0+0".format(
-            master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
-        master.bind('<Escape>',self.toggle_geom)
-
-        self.twitter = twitter.Twitter()
-
         self.grid()
         self.create_GUI()
 
+        self.csv_dict = self.readFile()
+
     def create_GUI(self):
-        Label(mainWindow, text="Message", width="10").grid(row=0, column=0)
+        Label(mainWindow, text="Bericht", width="10").grid(row=0, column=0)
 
         self.message = Entry(mainWindow, width="40")
         self.message.grid(row=0, column=1)
 
-        self.button = Button(mainWindow, text="Verzenden", command=self.onPressTweet, width="20")
-        self.button.grid(row=2, column=0, columnspan=2)
+        self.button = Button(mainWindow, text="Verzenden", command=self.onPressVerzenden, width="20")
+        self.button.grid(row=1, column=0, columnspan=2)
 
-        self.listbox = Listbox(mainWindow, width=100, height=22)
-        self.listbox.grid(row=4, column=0, columnspan=8)
 
-        self.updateListbox()
+    def onPressVerzenden(self):
 
-    def toggle_geom(self, event):
-        geom = self.master.winfo_geometry()
-        print(geom, self._geom)
-        self.master.geometry(self._geom)
-        self._geom = geom
+        message = self.message.get()
 
-    def updateListbox(self):
-        self.listbox.delete(0, END)
+        if len(message)<=140:
 
-        tweets = self.twitter.getFeed()
+        else:
+            print("FOUT")
 
-        for tweet in tweets:
-            self.listbox.insert(END, tweet['text'])
 
-    def onPressTweet(self):
+    def writeFile(self,d):
 
-        self.twitter.postTweet(self.message.get())
+        return
 
-        self.message.delete(0, END)
+    def readFile(self):
+        with open(CSV_PATH, 'r') as f:
+            reader = csv.reader(f)
+            d = dict(reader)
 
-        self.updateListbox()
-
+        return d
 
 mainWindow = Tk()
 mainWindow.title("Invoer scherm")
-#mainWindow.geometry("450x450")
+mainWindow.geometry("450x450")
 app = MainMenu(mainWindow)
 mainWindow.mainloop()
