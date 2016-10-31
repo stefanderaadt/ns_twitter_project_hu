9a407163ad69d705e7b5cb1c169b40b7b5672736
stefanderaadt <stefanderaadt@gmail.com>
2016-10-31 19:14:57 +0100

    Aanpassingen invoerscherm.py

--- invoerscherm.py
+++ invoerscherm.py
@@ -9,41 +9,41 @@ class MainMenu(Frame):
         self.grid()
         self.create_GUI()
 
-        self.csv_dict = self.readFile()
-
     def create_GUI(self):
         Label(mainWindow, text="Bericht", width="10").grid(row=0, column=0)
+        Label(mainWindow, text="Naam", width="10").grid(row=1, column=0)
 
         self.message = Entry(mainWindow, width="40")
         self.message.grid(row=0, column=1)
+        self.naam = Entry(mainWindow, width="40")
+        self.naam.grid(row=1, column=1)
 
         self.button = Button(mainWindow, text="Verzenden", command=self.onPressVerzenden, width="20")
-        self.button.grid(row=1, column=0, columnspan=2)
+        self.button.grid(row=2, column=0, columnspan=2)
 
 
     def onPressVerzenden(self):
 
         message = self.message.get()
+        naam = self.naam.get()
 
         if len(message)<=140:
-        
+            self.writeFile(["1",message,naam])
         else:
             print("FOUT")
 
 
-    def writeFile(self,d):
+    def writeFile(self, r):
+        with open(CSV_PATH, 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(r)
 
         return
 
-    def readFile(self):
-        with open(CSV_PATH, 'r') as f:
-            reader = csv.reader(f)
-            d = dict(reader)
-
-        return d
 
 mainWindow = Tk()
 mainWindow.title("Invoer scherm")
-mainWindow.geometry("450x450")
+# mainWindow.geometry("450x450")
+#mainWindow.attributes('-fullscreen', True)
 app = MainMenu(mainWindow)
 mainWindow.mainloop()
