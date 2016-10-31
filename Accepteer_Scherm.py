58200148b4b2f3b240abb9a0f41a1edc09c8a2d2
jeffrey <jeffrey.gek@hotmail.com>
2016-10-31 13:11:14 +0100

    Accepteer scherm toegevoegd

--- Accepteer_Scherm.py
+++ Accepteer_Scherm.py
@@ -1,1 +1,24 @@
-asa
+from tkinter import *
+
+class MainMenu(Frame):
+    def __init__(self, master):
+        super(MainMenu, self).__init__(master)
+
+        self.grid()
+        self.create_GUI()
+
+    def create_GUI(self):
+        Label(mainWindow, text="Message", width="10").grid(row=0, column=0)
+
+        self.button = Button(mainWindow, text="BUTTON", command=self.onPressTweet, width="20")
+        self.button.grid(row=2, column=0, columnspan=2)
+
+    def onPressTweet(self):
+
+
+
+mainWindow = Tk()
+mainWindow.title("Venster naam")
+mainWindow.geometry("450x450")
+MainMenu(mainWindow)
+mainWindow.mainloop()
