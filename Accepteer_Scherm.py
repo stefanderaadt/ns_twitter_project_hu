38d2fe54d6882a43145e4f894f49fd2072f93773
stefanderaadt <stefanderaadt@gmail.com>
2016-10-31 20:35:33 +0100

    Aanpassingen invoerscherm.py

--- Accepteer_Scherm.py
+++ Accepteer_Scherm.py
@@ -10,82 +10,85 @@ class MainMenu(Frame):
         self.grid()
         self.create_GUI()
         self.twitter = twitter.Twitter()
-        self.twitter.postTweet("bericht")
+        self.TweetOntvangen()
+        self.FormaatKiezen()
 
     def create_GUI(self):
 
-        self.label1 = Label(mainWindow, text=MainMessage()[0], width="2000",height = "1", background=KleurTweet(), anchor='w')
+        self.label1 = Label(mainWindow, text=self.MainMessage()[0], width="2000",height = "1", background=self.KleurTweet(), anchor='w')
         self.label1.grid(row=0, column=0, sticky=W)
-        self.label2 = Label(mainWindow, text=MainMessage()[1], width="2000",height = "1", background=KleurTweet(), anchor='w')
+        self.label2 = Label(mainWindow, text=self.MainMessage()[1], width="2000",height = "1", background=self.KleurTweet(), anchor='w')
         self.label2.grid(row=1, column=0, sticky=W)
-        for i in range(len(TweetOntvangen()[0])):
-            self.button = Button(mainWindow, text=(TweetOntvangen()[0][i]+ " ontvangen door, "+ TweetOntvangen()[1][i]), command=Onpress, width="2000", anchor='w')
+        for i in range(len(self.TweetOntvangen()[0])):
+            self.button = Button(mainWindow, text=(self.TweetOntvangen()[0][i]+ " ontvangen door, "+ self.TweetOntvangen()[1][i]), command=self.Onpress, width="2000", anchor='w')
 
             self.button.grid(row=2+i, column=0, sticky=W)
 
-def TweetOntvangen():
+    def TweetOntvangen(self):
 
 
-        with open("C:/Users/jeffrey/Dropbox/PycharmProjects/ns_twitter_project_hu2/data/tweets.csv ", "r") as MyCsvFile:
-            reader = csv.DictReader(MyCsvFile)
-            OntvangenTweets = []
-            GeplaatstDoor = []
-            for row in reader:
-                OntvangenTweets.append(row['tweet'])
-                GeplaatstDoor.append(row["plaatser"])
-        return OntvangenTweets,GeplaatstDoor
-def IsTweetOntvangen():
-    if len(TweetOntvangen()[0]) != 0:
-        Tweet = 1
-    else:
-        Tweet = 0
-    return Tweet
-
-def MainMessage():
-    bericht = ""
-    bericht1 = ''
-    if IsTweetOntvangen()== 0:
-        bericht = 'U heeft geen bericht ontvangen.'
-    if IsTweetOntvangen()== 1:
-        if len(TweetOntvangen()[0]) > 1:
-            bericht = "U heeft "+str(len(TweetOntvangen()[0]))+" berichten"
-            bericht1 = "Dit zijn de berichten:"
+            with open("data/tweets.csv ", "r") as MyCsvFile:
+                reader = csv.DictReader(MyCsvFile)
+                OntvangenTweets = []
+                GeplaatstDoor = []
+                for row in reader:
+                    OntvangenTweets.append(row['tweet'])
+                    GeplaatstDoor.append(row["plaatser"])
+            return OntvangenTweets,GeplaatstDoor
+    def IsTweetOntvangen(self):
+        if len(self.TweetOntvangen()[0]) != 0:
+            Tweet = 1
         else:
-            bericht = "U heeft een bericht."
-            bericht1 = "Dit is uw bericht:"
-    return bericht, bericht1
+            Tweet = 0
+        return Tweet
+
+    def MainMessage(self):
+        bericht = ""
+        bericht1 = ''
+        if self.IsTweetOntvangen()== 0:
+            bericht = 'U heeft geen bericht ontvangen.'
+        if self.IsTweetOntvangen()== 1:
+            if len(self.TweetOntvangen()[0]) > 1:
+                bericht = "U heeft "+str(len(self.TweetOntvangen()[0]))+" berichten"
+                bericht1 = "Dit zijn de berichten:"
+            else:
+                bericht = "U heeft een bericht."
+                bericht1 = "Dit is uw bericht:"
+        return bericht, bericht1
 
 
-def Onpress():
-    result = messagebox.askquestion("Tweet versturen", "Wilt u deze tweet versturen?", icon="warning")
-    if result == 'yes':
-        TweetNaarTwitter = 1
-    else:
-        TweetNaarTwitter = 0
-    return TweetNaarTwitter
+    def Onpress(self):
+        result = messagebox.askquestion("Tweet versturen", "Wilt u deze tweet versturen?", icon="warning")
+        if result == 'yes':
+            TweetNaarTwitter = 1
+            self.tw
+        else:
+            TweetNaarTwitter = 0
+        return TweetNaarTwitter
 
-def FormaatKiezen():
-    # screen = str(input("screenformaat? Je kunt invullen:\nFullscreen\nFormaat in HxB bijvoorbeeld 1920x1080\n")).lower()
-    # if screen == 'fullscreen':
-    #     return mainWindow.attributes('-fullscreen', True)
-    # else:
-        return mainWindow.geometry('500x500+1920+200')
-def KleurTweet():
-    if IsTweetOntvangen()== 0:
-        kleur = "red"
-    else:
-        kleur = "green"
-    return kleur
+    def FormaatKiezen(self):
+        # screen = str(input("screenformaat? Je kunt invullen:\nFullscreen\nFormaat in HxB bijvoorbeeld 1920x1080\n")).lower()
+        # if screen == 'fullscreen':
+        #     return mainWindow.attributes('-fullscreen', True)
+        # else:
+            #return mainWindow.geometry('500x500+1920+200')
+            return mainWindow.geometry('500x500')
+    def KleurTweet(self):
+        if self.IsTweetOntvangen()== 0:
+            kleur = "red"
+        else:
+            kleur = "green"
+        return kleur
 
 
 
 
-TweetOntvangen()
+
 mainWindow = Tk()
 mainWindow.title("TweetsCheck MainPanel")
-FormaatKiezen()
+
 MainMenu(mainWindow)
-mainWindow.configure(background=KleurTweet())
+mainWindow.configure(background="#ffffff")#KleurTweet())
 mainWindow.mainloop()
 
 
