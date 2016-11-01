4af7f82e8e2e09e8960c6a42be7245d9f4d3ef68
Stefan <stefanderaadt@gmail.com>
2016-11-01 13:03:17 +0100

    Probleem niet opgelost

--- Accepteer_Scherm.py
+++ Accepteer_Scherm.py
@@ -21,7 +21,6 @@ class MainMenu(Frame):
         self.FormaatKiezen()
         self.refresh()
 
-
     def create_GUI(self):
         r = ''
         self.label1 = Label(mainWindow, text=self.MainMessage()[0], width="2000", height="1",
@@ -30,18 +29,19 @@ class MainMenu(Frame):
         self.label2 = Label(mainWindow, text=self.MainMessage()[1], width="2000", height="1",
                             background=self.KleurTweet(), anchor='w')
         self.label2.grid(row=1, column=0, sticky=W)
-    def refresh(self):
-        #self.button
-        for b in self.button:
-            b.grid_forget()
 
+    def refresh(self):
         self.list = self.TweetOntvangen()
+
+        for b in self.button:
+            b.destroy()
+
+        self.button = []
 
         for i in range(len(self.list)):
             self.button.append(Button(mainWindow,
-                                 text=(self.list[i][0] + " ontvangen door, " + self.list[i][1]),
-                                 command= lambda i=i: self.Onpress(i), width="2000", anchor='w'))
-            print(self.list[i][0])
+                                      text=(self.list[i][0] + " ontvangen door, " + self.list[i][1]),
+                                      command=lambda i=i: self.Onpress(i), width="2000", anchor='w'))
 
             self.button[i].grid(row=2 + i, column=0, sticky=W)
 
@@ -53,7 +53,6 @@ class MainMenu(Frame):
             for row in reader:
                 OntvangenTweets.append([row['tweet'], row['plaatser']])
         return OntvangenTweets
-
 
     def IsTweetOntvangen(self):
         if len(self.TweetOntvangen()) != 0:
@@ -79,9 +78,9 @@ class MainMenu(Frame):
     def Onpress(self, i):
         result = messagebox.askquestion("Tweet versturen", "Wilt u deze tweet versturen?", icon="warning")
         if result == 'yes':
-            #self.twitter.postTweet(self.list[i][0])
+
+            # self.twitter.postTweet(self.list[i][0])
             self.list.remove(self.list[i])
-            print(self.list)
             self.TweetVerwijderen()
             self.refresh()
         else:
@@ -90,20 +89,19 @@ class MainMenu(Frame):
             return
 
     def TweetVerwijderen(self):
-        #with open("data/tweets.csv", "w") as MyCsvFile:
-            #fieldnames  = ['tweet', 'plaatser']
-            #writer = csv.DictWriter(MyCsvFile, fieldnames=fieldnames)
-            #writer.writeheader()
-            #print(self.list)
-            #for i in range(len(self.list)):
-            #    writer.writerow(self.list[i][0])
+        # with open("data/tweets.csv", "w") as MyCsvFile:
+        # fieldnames  = ['tweet', 'plaatser']
+        # writer = csv.DictWriter(MyCsvFile, fieldnames=fieldnames)
+        # writer.writeheader()
+        # print(self.list)
+        # for i in range(len(self.list)):
+        #    writer.writerow(self.list[i][0])
 
         with open(CSV_PATH, 'w', newline='') as f:
-            writer = csv.writer(f , delimiter=',')
+            writer = csv.writer(f, delimiter=',')
             writer.writerow(['tweet', 'plaatser'])
             for row in self.list:
                 writer.writerow(row)
-
 
     def FormaatKiezen(self):
         # screen = str(input("screenformaat? Je kunt invullen:\nFullscreen\nFormaat in HxB bijvoorbeeld 1920x1080\n")).lower()
@@ -111,7 +109,6 @@ class MainMenu(Frame):
         #     return mainWindow.attributes('-fullscreen', True)
         # else:
         return mainWindow.geometry('500x500+200+200')
-
 
     def KleurTweet(self):
         if self.IsTweetOntvangen() == 0:
