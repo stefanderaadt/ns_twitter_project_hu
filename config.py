6b2c0a499045f5a95d6d1bc9c6df18cdac9a9177
Stefan <stefanderaadt@gmail.com>
2016-11-01 14:32:19 +0100

    config bestand

---
+++ config.py
@@ -0,0 +1,10 @@
+CONSUMER_KEY = "e4EqizRbyJOV8ohitSyzpaNJ3"
+CONSUMER_SECRET = "VGRzamr4j5SD66wWc5LTOsIsXctV0TozroEudAQ8gwrUs3xBMX"
+ACCESS_KEY = "791932465845678081-IFyvMJXcqCkzQxP7ClggyqwKECXkhpb"
+ACCESS_SECRET = "NiEb4nvC8zgDf1TP90IQmDnzmQHNLpjLsiskw0YLjSCZg"
+
+USER = "ns_test_account"
+
+CSV_PATH = "data/tweets.csv"
+IMG_PATH = "picture/"
+SOUND_PATH = "sound/"
